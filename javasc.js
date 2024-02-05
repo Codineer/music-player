b0cf77f667e76daa70bec73c8347c494d257fdbd
Codineer <utkarshgamer294@outlook.com>
2024-02-06 02:01:16 +0530

    another commit

--- javasc.js
+++ javasc.js
@@ -1,10 +1,54 @@
-async function fd(){
-    return "hii"
-    
-}
-function d(){
-    console.log("mii")
+const audio = new Audio('songs/Hass Hass.mp3');
+
+// Add an event listener for when the metadata has loaded
+audio.addEventListener('loadedmetadata', function() {
+  // Access the metadata
+  const artist = audio.artist || getMetadata(audio, 'artist');
+
+  if (artist) {
+    console.log('Artist:', artist);
+  } else {
+    console.log('Artist information not available.');
+  }
+});
+
+// Function to retrieve metadata from the Web Audio API
+function getMetadata(audio, key) {
+  const metadata = audio[key];
+  if (metadata) {
+    return metadata;
+  }
+
+  const mediaSource = new MediaSource();
+  const mediaSourceURL = URL.createObjectURL(mediaSource);
+  const sourceBuffer = mediaSource.addSourceBuffer('songs/Hass Hass.mp3');
+
+  sourceBuffer.addEventListener('updateend', function() {
+    mediaSource.endOfStream();
+
+    // Access the metadata
+    const artist = audio[key] || 'Unknown Artist';
+
+    // Cleanup
+    URL.revokeObjectURL(mediaSourceURL);
+    audio[key] = artist;
+
+    // Trigger loadedmetadata event manually
+    audio.dispatchEvent(new Event('loadedmetadata'));
+  });
+
+  // Set the source buffer
+  sourceBuffer.appendBuffer(fetchAudioFile('songs/Hass Hass.mp3'));
+
+  return null;
 }
 
-console.log(fd())
-d()
+// Function to fetch the audio file
+async function fetchAudioFile(url) {
+  const response = await fetch(url);
+  const buffer = await response.arrayBuffer();
+  return buffer;
+}
+
+// Start loading the audio
+audio.load();
--- script.js
+++ script.js
@@ -1,9 +1,68 @@
 console.log("lets write javascript")
-async function main() {
+
+async function addliinlibrary(tds){
+    let song = []
+    
+    for (let i = 0; i < tds.length; i++) {
+        if (tds[i].href.endsWith(".mp3")) {
+            let songname = tds[i].querySelector('.name').innerText
+            let li = document.createElement("li")
+            li.innerHTML = `
+            <img src="icon.svg" alt="">
+            <div class="sname">
+                ${songname}
+            </div>
+            <div class="artist">
+                Song artist
+            </div>
+            <img src="play.svg" class="invert alplay" alt="">
+            <img src="playicon.svg" class="hplay invert">
+        `
+            document.querySelector('.songList').querySelector('ul').appendChild(li)
+            song.push(tds[i].href)
+        }
+    }
+    console.log(2)
+    return song
+} 
+async function getSongs() {
     let a = await fetch("http://127.0.0.1:5500/songs/")
     let response = await a.text()
-    console.log(response)
-    let element=document.createElement("div")
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let tds = div.getElementsByTagName("a")
+    console.log(1)
+    return await addliinlibrary(tds)
+}
+
+async function main() {
+    //geting list
+    let songs = await getSongs()
+    console.log(3)
+    let songli=document.querySelector('.songList').querySelectorAll('li') 
+    songli.forEach((li)=>{ 
+    li.addEventListener('mouseover', function () {
+        songimgplay = li.querySelector('.hplay')
+        // Your code to execute when the element is hovered
+        songimgplay.style.display = "block";
+     
+    });
+    li.addEventListener('mouseout', function () {
+        songimgplay = li.querySelector('.hplay')
+        // Your code to execute when the element is hovered
+        songimgplay.style.display = "none";
+      
+    });})
+    let songlistbox=document.querySelector('.songList').querySelector('ul');
+    console.log(songlistbox)
+    
+    //play the first song
+    var audio = new Audio(songs[0]);
+    // audio.play();
+    audio.addEventListener("loadeddata", () => {
+        console.log(audio.duration, audio.currentSrc, audio.currentTime)
+        // The duration variable now holds the duration (in seconds) of the audio clip
+    });
 
 }
 main()
