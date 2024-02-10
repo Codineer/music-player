30cff8ab15350d5b278ef778cf7917762cf9e965
Codineer <utkarshgamer294@outlook.com>
2024-02-10 23:33:31 +0530

    another commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// import req
 console.log("lets write javascript")
 let currentSong = new Audio();
 currentSong.volume = 0;
@@ -14,20 +15,20 @@ function playMusic(li, link, currentsongi) {
             li.querySelector('.sname').innerText;
         document.querySelector('.songtime').innerText = "0:00/0:00"
         let playpromise = currentSong.play()
-        playpromise.then(() => document.querySelector("#play").src = "playing.svg")
+        playpromise.then(() => document.querySelector("#play").src = "images/playing.svg")
     }
 
 }
 
 function pauseResume() {
     if (currentSong.paused) {
-        currentSong.play().then(() => { document.querySelector("#play").src = "playing.svg" })
+        currentSong.play().then(() => { document.querySelector("#play").src = "images/playing.svg" })
 
     }
     else {
         currentSong.pause()
 
-        document.querySelector("#play").src = "play.svg"
+        document.querySelector("#play").src = "images/play.svg"
     }
 }
 
@@ -40,7 +41,7 @@ async function addliinlibrary(tds) {
             let songname = tds[i].querySelector('.name').innerText
             let li = document.createElement("li")
             li.innerHTML = `
-            <img src="icon.svg" alt="">
+            <img src="images/icon.svg" alt="">
             <div class="sname" dataComment=${tds[i].href} >
                 ${songname}
             </div>
@@ -48,8 +49,8 @@ async function addliinlibrary(tds) {
             <div class="artist">
                 Song artist
             </div>
-            <img src="play.svg" class="invert alplay" alt="">
-            <img src="playicon.svg" class="hplay invert">
+            <img src="images/play.svg" class="invert alplay" alt="">
+            <img src="images/playicon.svg" class="hplay invert">
         `
             li.addEventListener('mouseover', function () {
                 let songimgplay = li.querySelector('.hplay')
@@ -78,7 +79,7 @@ async function addliinlibrary(tds) {
 }
 async function getSongs(album) {
 
-    let a = await fetch(`http://127.0.0.1:5500/albums/${album}/`)
+    let a = await fetch(album)
     let response = await a.text()
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -134,26 +135,72 @@ function changeVolume(offsetY) {
 
 }
 
-async function displayalbums(){
+async function displayalbums() {
     let albums = await fetch('http://127.0.0.1:5500/albums')
-    let response =await albums.text()
-    let div =document.createElement('div')
+    let response = await albums.text()
+    let div = document.createElement('div')
     div.innerHTML = response
-    let alist=[];
+    let alist = [];
     div.querySelectorAll('a')
-    .forEach(element => {
-        // console.log(element.href)
-        if (element.href.startsWith("http://127.0.0.1:5500/albums/")){
-            alist.push(element.href)
-        }
-    });
-    
+        .forEach(element => {
+            // console.log(element.href)
+            if (element.href.startsWith("http://127.0.0.1:5500/albums/")) {
+                alist.push(element.href)
+            }
+        });
+    let container = document.querySelector(".cardContainer")
     console.log(alist)
+    for (let element of alist){
+    
+        let card = document.createElement("div")
+        card.classList.add('card');
+        card.classList.add('rounded');
+        
+        let filePath = `${element}/info.json`
+        let response =await fetch(filePath)
+        let jsonContent = await response.json()
+        
+        card.innerHTML = `<div data-folder="${element}" class="imgincard">
+        <div class="play">
+
+            <svg height="50px" width="50px" version="1.1" id="Capa_1"
+                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 58 58" xml:space="preserve" fill="#000000">
+
+                <g id="SVGRepo_bgCarrier" stroke-width="0" />
+
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+
+                <g id="SVGRepo_iconCarrier">
+                    <circle style="fill:#ff0000;" cx="29" cy="29" r="29" />
+                    <g>
+                        <polygon style="fill:#ffffff;" points="40,29 22,39 22,29.273 22,19 " />
+                        <path style="fill:#ff0000;"
+                            d="M22,45c-0.16,0-0.321-0.038-0.467-0.116C21.205,44.711,21,44.371,21,44V14 c0-0.371,0.205-0.711,0.533-0.884c0.328-0.174,0.724-0.15,1.031,0.058l22,15C44.836,28.36,45,28.669,45,29s-0.164,0.64-0.437,0.826 l-22,15C22.394,44.941,22.197,45,22,45z M23,15.893v26.215L42.225,29L23,15.893z" />
+                    </g>
+                </g>
+
+            </svg>
+
+
+
+        </div>
+        <img class="rounded"
+            src="${element}/cover.jpg" alt="">
+    </div>
+    <h2>${jsonContent.title}</h2>
+    <p>${jsonContent.description}</p>`
+    card.addEventListener('click',(event) => {
+        getSongs(event.currentTarget.querySelector(".imgincard").getAttribute('data-folder'))
+    })
+        container.appendChild(card)
+    };
+
 }
 async function main() {
 
     //geting list
-    let songs = await getSongs("phonk")
+    let songs = await getSongs("http://127.0.0.1:5500/albums/phonk")
     console.log(songs)
     await displayalbums()
 
@@ -168,20 +215,23 @@ async function main() {
     document.querySelector('.seekbar').addEventListener('click', e => {
         console.log(currentSong.currentTime = (((e.offsetX + 1) / (parseInt(document.querySelector('.seekbar').getBoundingClientRect().width))) * currentSong.duration));
     })
-    document.querySelector('.card').addEventListener('mouseenter', (event) => {
-        event.target.style.background = "#272727"
-        event.target.querySelector('img').style.boxShadow = "3px 3px 20px 3px #121212"
-        event.target.querySelector('.play').style.bottom = "8%"
-        event.target.querySelector('.play').style.opacity = "1"
-
-    })
-    document.querySelector('.card').addEventListener('mouseleave', (event) => {
-        event.target.style.background = "#181818"
-        event.target.querySelector('img').style.boxShadow = "0px 0px 0px 0px #121212"
-        event.target.querySelector('.play').style.bottom = "2%"
-        event.target.querySelector('.play').style.opacity = "0"
-
-    })
+    document.querySelectorAll('.card').forEach(element => {
+        element.addEventListener('mouseenter', (event) => {
+            event.target.style.background = "#272727"
+            event.target.querySelector('img').style.boxShadow = "3px 3px 20px 3px #121212"
+            event.target.querySelector('.play').style.bottom = "8%"
+            event.target.querySelector('.play').style.opacity = "1"
+    
+        })
+        element.addEventListener('mouseleave', (event) => {
+            event.target.style.background = "#181818"
+            event.target.querySelector('img').style.boxShadow = "0px 0px 0px 0px #121212"
+            event.target.querySelector('.play').style.bottom = "2%"
+            event.target.querySelector('.play').style.opacity = "0"
+    
+        })
+    });
+   
 
     //add event listeners to volume slider
 
@@ -211,10 +261,6 @@ async function main() {
         document.querySelector(".left").style.left = "-100%"
     })
     console.log(songrecord)
-
-    setTimeout(() => {
-        getSongs("relax")
-    }, 4000);
 
 }
 
